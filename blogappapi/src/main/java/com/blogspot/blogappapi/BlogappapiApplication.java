c260d1d16d8bb05a86e72852a68a310397e039d6
jadhavabhishek140 <71170216+jadhavabhishek140@users.noreply.github.com>
2022-08-03 15:04:05 +0530

    Created Spring Boot Project for Backend

---
+++ blogappapi/src/main/java/com/blogspot/blogappapi/BlogappapiApplication.java
@@ -0,0 +1,13 @@
+package com.blogspot.blogappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogappapiApplication.class, args);
+	}
+
+}
---
+++ blogappapi/src/test/java/com/blogspot/blogappapi/BlogappapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogspot.blogappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
